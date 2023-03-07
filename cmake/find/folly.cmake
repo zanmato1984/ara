189174c48bc020b5a34b73868a700b02a0b02c8c
zanmato <zanmato1984@gmail.com>
2023-03-07 22:14:14 +0800

    Refine dependencies

---
+++ cmake/find/folly.cmake
@@ -0,0 +1,4 @@
+message(STATUS "Finding Folly")
+
+set(Folly_FIND_QUIETLY 0)
+find_package(folly REQUIRED)
---
+++ cmake/find/gflags.cmake
@@ -0,0 +1,4 @@
+message(STATUS "Finding GFlags")
+
+set(GFlags_FIND_QUIETLY 0)
+find_package(GFlags REQUIRED)
