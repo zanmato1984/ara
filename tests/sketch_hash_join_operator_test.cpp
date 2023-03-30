1f47cbd3d86038f5c52999d3c21075811edc0210
zanmato <zanmato1984@gmail.com>
2023-03-30 21:37:47 +0800

    Add sketch hash join operator test cpp

---
+++ tests/sketch_hash_join_operator_test.cpp
@@ -0,0 +1,7 @@
+// TODO: Case about operator chaining in a pipeline.
+// NOTE: How to implement hash join probe operator in a parallel-agnostic and future-agnostic fashion.  That is:
+// 1. How to invoke and chain the `Stream()` methods of all the operators in this pipeline.
+// 2. How to invoke the `Finish()` methods of all the operators in this pipeline.
+// This is critical for cases like a pipeline having two hash right outer join probe operators,
+// the `Finish()` method of each will scan the hash table and produce non-joined rows.
+// 3. How to turn the sync `Stream()` and `Finish()` methods to future.
