d0870420cb1fa1e14ad15e2d74a2e2ec3ac5a7c5
zanmato1984 <zanmato1984@gmail.com>
2023-04-10 02:45:01 +0800

    Change per-build-task granularity to per-block rather than per-thread

--- tests/sketch_operator_test_copy.cpp
+++ tests/sketch_operator_test_copy.cpp
@@ -40,99 +40,124 @@ struct OperatorResult {
   std::optional<Batch> batch;
 };
 
-class TasksIterator {
+class TaskGroups {
  public:
-  using Task = std::function<arrow::Status(size_t)>;
-  using Tasks = std::vector<Task>;
+  using Task = std::function<OperatorResult(size_t /*round*/)>;
+  using TaskGroup = std::vector<std::pair<Task, size_t /*num_rounds*/>>;
 
-  virtual ~TasksIterator() = default;
+  virtual ~TaskGroups() = default;
 
   virtual bool HasNext() = 0;
-  virtual arrow::Result<Tasks> Next() = 0;
+  virtual arrow::Result<TaskGroup> Next() = 0;
 };
 
 class Operator {
  public:
   virtual ~Operator() = default;
 
-  virtual OperatorResult Push(size_t thread_id, const Batch & batch) {
+  virtual OperatorResult Push(size_t thread_id, const Batch& batch) {
     return {OperatorStatus::RUNNING(), {}};
   }
-  virtual std::unique_ptr<TasksIterator> Break(size_t dop) { return nullptr; }
-  virtual OperatorResult Finish(size_t thread_id) { return {OperatorStatus::RUNNING(), {}}; }
+  virtual arrow::Result<std::unique_ptr<TaskGroups>> Break(size_t dop) { return nullptr; }
+  virtual OperatorResult Finish(size_t thread_id) {
+    return {OperatorStatus::RUNNING(), {}};
+  }
 };
 
 TEST(OperatorTest, HashJoinBuild) {
-  class HashJoinBuildTasksIterator : public TasksIterator {
+  class BuildTaskGroups : public TaskGroups {
    public:
-    explicit HashJoinBuildTasksIterator(
-        std::shared_ptr<arrow::compute::HashJoinImpl>& join_impl, size_t dop)
+    explicit BuildTaskGroups(arrow::compute::HashJoinImpl* join_impl, size_t dop)
         : join_impl(join_impl),
           dop(dop),
           count(0),
-          current_task_group(-1),
-          current_num_tasks(0),
-          next_task_cont(
+          current_build_task_group(-1),
+          current_num_build_tasks(0),
+          next_build_task_cont(
               [this](size_t) { return this->join_impl->BuildHashTable(0, {}, {}); }) {}
 
-    bool HasNext() override { return count < task_groups.size(); }
+    bool HasNext() override { return count < build_task_groups.size(); }
 
-    arrow::Result<Tasks> Next() override {
-      ARROW_RETURN_NOT_OK(next_task_cont(0));
-      auto it = task_groups.find(current_task_group);
-      ARROW_RETURN_IF(it == task_groups.end(), arrow::Status::Invalid("Task group not found"));
-      auto& task = it->second.first;
-      auto& task_cont = it->second.second;
+    arrow::Result<TaskGroup> Next() override {
+      ARROW_RETURN_NOT_OK(next_build_task_cont(0));
+      auto it = build_task_groups.find(current_build_task_group);
+      ARROW_RETURN_IF(it == build_task_groups.end(),
+                      arrow::Status::Invalid("Build task group not found"));
+      auto& build_task = it->second.first;
+      auto& build_task_cont = it->second.second;
 
-      Tasks tasks(dop);
+      TaskGroup task_group;
+      task_group.reserve(current_num_build_tasks);
+
       // Spread tasks across threads.
+      auto num_tasks_per_thread = (current_num_build_tasks + dop - 1) / dop;
+      for (size_t build_task_id = 0; build_task_id < current_num_build_tasks;
+           build_task_id += num_tasks_per_thread) {
+        auto thread_id = task_group.size();
+        auto num_tasks =
+            std::min(num_tasks_per_thread, current_num_build_tasks - build_task_id);
+        task_group.emplace_back(
+            [build_task, build_task_cont, num_tasks_per_thread, build_task_id, thread_id,
+             num_tasks](size_t round) {
+              auto status =
+                  build_task(thread_id, thread_id * num_tasks_per_thread + round);
+              if (status.ok()) {
+                return OperatorResult{OperatorStatus::RUNNING(), {}};
+              } else {
+                return OperatorResult{OperatorStatus::ERROR(std::move(status)), {}};
+              }
+            },
+            num_tasks);
+      }
 
       count++;
-      next_task_cont = task_cont;
-      return arrow::Result(std::move(tasks));
+      next_build_task_cont = build_task_cont;
+      return arrow::Result(std::move(task_group));
     }
 
    private:
-    std::shared_ptr<arrow::compute::HashJoinImpl> join_impl;
+    arrow::compute::HashJoinImpl* join_impl;
 
    private:
-    using Task = std::function<arrow::Status(size_t, int64_t)>;
-    using TaskCont = std::function<arrow::Status(size_t)>;
-    using TaskGroup = std::pair<Task, TaskCont>;
+    using BuildTask = std::function<arrow::Status(size_t, int64_t)>;
+    using BuildTaskCont = std::function<arrow::Status(size_t)>;
+    using BuildTaskGroup = std::pair<BuildTask, BuildTaskCont>;
 
-    int RegisterTaskGroup(Task task, TaskCont task_cont) {
-      int id = task_groups.size();
-      task_groups.emplace(id, std::make_pair(std::move(task), std::move(task_cont)));
+    int RegisterTaskGroup(BuildTask build_task, BuildTaskCont build_task_cont) {
+      int id = build_task_groups.size();
+      build_task_groups.emplace(
+          id, std::make_pair(std::move(build_task), std::move(build_task_cont)));
       return id;
     }
 
-    arrow::Status StartTaskGroup(int task_group, int64_t num_tasks) {
-      current_task_group = task_group;
-      current_num_tasks = num_tasks;
+    arrow::Status StartTaskGroup(int build_task_group, int64_t num_build_tasks) {
+      current_build_task_group = build_task_group;
+      current_num_build_tasks = num_build_tasks;
       return arrow::Status::OK();
     }
 
-    private:
+   private:
     const size_t dop;
-    std::unordered_map<int, TaskGroup> task_groups;
+    std::unordered_map<int, BuildTaskGroup> build_task_groups;
     size_t count;
-    int current_task_group;
-    int64_t current_num_tasks;
-    TaskCont next_task_cont;
+    int current_build_task_group;
+    int64_t current_num_build_tasks;
+    BuildTaskCont next_build_task_cont;
   };
 
-  class HashJoinBuild : public Operator {
+  class HashJoinBuildOperator : public Operator {
    public:
-    explicit HashJoinBuild(std::shared_ptr<arrow::compute::HashJoinImpl>& join_impl, size_t dop)
-        : join_impl(join_impl), task_iterator(std::make_unique<HashJoinBuildTasksIterator>(join_impl, dop)) {}
-    ~HashJoinBuild() override = default;
+    ~HashJoinBuildOperator() override = default;
 
-    std::unique_ptr<TasksIterator> Break(size_t dop) override {
-      return std::move(task_iterator);
+    arrow::Result<std::unique_ptr<TaskGroups>> Break(size_t dop) override {
+      auto join_impl_ = arrow::compute::HashJoinImpl::MakeSwiss();
+      // TODO: Init join_impl_.
+      ARROW_RETURN_NOT_OK(join_impl_);
+      join_impl = std::move(*join_impl_);
+      return std::make_unique<BuildTaskGroups>(join_impl.get(), dop);
     }
 
    private:
-    std::shared_ptr<arrow::compute::HashJoinImpl> join_impl;
-    std::unique_ptr<TasksIterator> task_iterator;
+    std::shared_ptr<arrow::compute::HashJoinImpl> join_impl = nullptr;
   };
 }
