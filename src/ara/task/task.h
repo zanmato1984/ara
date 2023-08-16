543fb796c8d1be2404ea944a980993d0c8fd68dc
zanmato1984 <zanmato1984@gmail.com>
2023-08-17 01:17:25 +0800

    WIP

--- src/ara/task/task.h
+++ src/ara/task/task.h
@@ -2,23 +2,38 @@
 
 #include <ara/common/defines.h>
 #include <ara/task/task_context.h>
+#include <ara/task/task_observer.h>
 #include <ara/task/task_status.h>
 
 namespace ara::task {
 
-using TaskInstanceId = size_t;
+using TaskId = size_t;
 using TaskResult = Result<TaskStatus>;
 
-using TaskImpl = std::function<TaskResult(const TaskContext&, TaskInstanceId)>;
+namespace detail {
 
+template <typename... Args>
 class Task {
+ private:
+  using Impl = std::function<TaskResult(Args...)>;
+
  public:
-  TaskResult operator()(const TaskContext& context, TaskInstanceId id) const {
-    return impl_(context, id);
-  }
+  Task(Impl impl, std::string name, std::string desc)
+      : impl_(std::move(impl)), name_(std::move(name)), desc_(std::move(desc)) {}
+
+  TaskResult operator()(Args... args) const { return impl_(std::forward<Args>(args)...); }
 
  private:
-  TaskImpl impl_;
+  Impl impl_;
+  std::string name_;
+  std::string desc_;
+
+  friend class TaskObserver;
 };
+
+}  // namespace detail
+
+using Task = detail::Task<std::function<TaskResult(const TaskContext&, TaskId)>>;
+using Cont = detail::Task<std::function<TaskResult(const TaskContext&)>>;
 
 };  // namespace ara::task
--- src/ara/task/task_context.h
+++ src/ara/task/task_context.h
@@ -1,7 +1,13 @@
 #pragma once
 
+#include <ara/task/backpressure.h>
+#include <ara/task/task_observer.h>
+
 namespace ara::task {
 
-struct TaskContext {};
+struct TaskContext {
+  BackpressurePairFactory backpressure_pair_factory;
+  std::optional<TaskObserver> observer;
+};
 
 };  // namespace ara::task
