f22895f4e976bd25984347271371c815a42cdc77
zanmato1984 <zanmato1984@gmail.com>
2023-08-26 01:06:17 +0800

    Refine schedule test using typed test

--- src/ara/schedule/schedule_test.cpp
+++ src/ara/schedule/schedule_test.cpp
@@ -13,94 +13,72 @@ using namespace ara;
 using namespace ara::task;
 using namespace ara::schedule;
 
-TaskResult ScheduleTask(const ScheduleContext& schedule_context, Scheduler& scheduler,
-                        Task task, size_t num_tasks, std::optional<Continuation> cont,
-                        std::optional<TaskGroup::NotifyFinishFunc> notify_finish) {
-  TaskGroup task_group("", "", std::move(task), num_tasks, std::move(cont),
-                       std::move(notify_finish));
-  auto handle = scheduler.Schedule(schedule_context, task_group);
-  ARA_RETURN_NOT_OK(handle);
-  return (*handle)->Wait(schedule_context);
-}
+struct AsyncDoublePoolSchedulerHolder {
+  folly::CPUThreadPoolExecutor cpu_executor{4};
+  folly::IOThreadPoolExecutor io_executor{2};
+  AsyncDoublePoolScheduler scheduler{&cpu_executor, &io_executor};
+};
 
-void ScheduleEmptyTask(const ScheduleContext& schedule_context, Scheduler& scheduler) {
+struct NaiveParallelSchedulerHolder {
+  NaiveParallelScheduler scheduler;
+};
+
+template <typename SchedulerType>
+class ScheduleTest : public testing::Test {
+ protected:
+  TaskResult ScheduleTask(const ScheduleContext& schedule_context, Task task,
+                          size_t num_tasks, std::optional<Continuation> cont,
+                          std::optional<TaskGroup::NotifyFinishFunc> notify_finish) {
+    TaskGroup task_group("", "", std::move(task), num_tasks, std::move(cont),
+                         std::move(notify_finish));
+    SchedulerType holder;
+    auto handle = holder.scheduler.Schedule(schedule_context, task_group);
+    ARA_RETURN_NOT_OK(handle);
+    return (*handle)->Wait(schedule_context);
+  }
+};
+
+using SchedulerTypes =
+    ::testing::Types<AsyncDoublePoolSchedulerHolder, NaiveParallelSchedulerHolder>;
+TYPED_TEST_SUITE(ScheduleTest, SchedulerTypes);
+
+TYPED_TEST(ScheduleTest, EmptyTask) {
+  ScheduleContext schedule_context;
   Task task("Task", "Do nothing", [](const TaskContext&, TaskId) -> TaskResult {
     return TaskStatus::Finished();
   });
-  auto result = ScheduleTask(schedule_context, scheduler, std::move(task), 4,
-                             std::nullopt, std::nullopt);
+  auto result = this->ScheduleTask(schedule_context, std::move(task), 4, std::nullopt,
+                                   std::nullopt);
   ASSERT_OK(result);
   ASSERT_TRUE(result->IsFinished());
 }
 
-TEST(ScheduleTest, AsyncDoublePoolSchedulerEmptyTask) {
+TYPED_TEST(ScheduleTest, EmptyTaskWithEmptyCont) {
   ScheduleContext schedule_context;
-  folly::CPUThreadPoolExecutor cpu_executor(4);
-  folly::IOThreadPoolExecutor io_executor(2);
-  AsyncDoublePoolScheduler scheduler(&cpu_executor, &io_executor);
-  ScheduleEmptyTask(schedule_context, scheduler);
-}
-
-TEST(ScheduleTest, NaiveParallelSchedulerEmptyTask) {
-  ScheduleContext schedule_context;
-  NaiveParallelScheduler scheduler;
-  ScheduleEmptyTask(schedule_context, scheduler);
-}
-
-void ScheduleEmptyTaskWithCont(const ScheduleContext& schedule_context,
-                               Scheduler& scheduler) {
   Task task("Task", "Do nothing", [](const TaskContext&, TaskId) -> TaskResult {
     return TaskStatus::Finished();
   });
   Continuation cont("Cont", "Do nothing", [](const TaskContext&) -> TaskResult {
     return TaskStatus::Finished();
   });
-  auto result = ScheduleTask(schedule_context, scheduler, std::move(task), 4,
-                             std::move(cont), std::nullopt);
+  auto result = this->ScheduleTask(schedule_context, std::move(task), 4, std::move(cont),
+                                   std::nullopt);
   ASSERT_OK(result);
   ASSERT_TRUE(result->IsFinished());
 }
 
-TEST(ScheduleTest, AsyncDoublePoolSchedulerEmptyTaskWithCont) {
+TYPED_TEST(ScheduleTest, EndlessTaskWithNotifyFinish) {
   ScheduleContext schedule_context;
-  folly::CPUThreadPoolExecutor cpu_executor(4);
-  folly::IOThreadPoolExecutor io_executor(2);
-  AsyncDoublePoolScheduler scheduler(&cpu_executor, &io_executor);
-  ScheduleEmptyTaskWithCont(schedule_context, scheduler);
-}
-
-TEST(ScheduleTest, NaiveParallelSchedulerEmptyTaskWithCont) {
-  ScheduleContext schedule_context;
-  NaiveParallelScheduler scheduler;
-  ScheduleEmptyTaskWithCont(schedule_context, scheduler);
-}
-
-void ScheduleEndlessTaskWithNotifyFinish(const ScheduleContext& schedule_context,
-                                         Scheduler& scheduler) {
   std::atomic_bool finished = false;
   Task task("Task", "Endless", [&](const TaskContext&, TaskId) -> TaskResult {
     return finished ? TaskStatus::Finished() : TaskStatus::Continue();
   });
-  auto result = ScheduleTask(schedule_context, scheduler, std::move(task), 4,
-                             std::nullopt, [&](const TaskContext&) {
-                               std::this_thread::sleep_for(std::chrono::seconds(1));
-                               finished = true;
-                               return Status::OK();
-                             });
+  auto result = this->ScheduleTask(schedule_context, std::move(task), 4, std::nullopt,
+                                   [&](const TaskContext&) {
+                                     std::this_thread::sleep_for(std::chrono::seconds(1));
+                                     finished = true;
+                                     return Status::OK();
+                                   });
   ASSERT_OK(result);
   ASSERT_TRUE(result->IsFinished());
-}
-
-TEST(ScheduleTest, AsyncDoublePoolSchedulerEndlessTaskWithNotifyFinish) {
-  ScheduleContext schedule_context;
-  folly::CPUThreadPoolExecutor cpu_executor(4);
-  folly::IOThreadPoolExecutor io_executor(2);
-  AsyncDoublePoolScheduler scheduler(&cpu_executor, &io_executor);
-  ScheduleEndlessTaskWithNotifyFinish(schedule_context, scheduler);
-}
-
-TEST(ScheduleTest, NaiveParallelSchedulerEndlessTaskWithNotifyFinish) {
-  ScheduleContext schedule_context;
-  NaiveParallelScheduler scheduler;
-  ScheduleEndlessTaskWithNotifyFinish(schedule_context, scheduler);
 }
