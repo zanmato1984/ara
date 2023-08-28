ec81d00b13276057cce4901ef144b4659bdac2f0
zanmato1984 <zanmato1984@gmail.com>
2023-08-29 01:23:44 +0800

    Add explain

--- src/ara/pipeline/logical_pipeline.cpp
+++ src/ara/pipeline/logical_pipeline.cpp
@@ -1,10 +1,23 @@
 #include "logical_pipeline.h"
 
+#include <sstream>
+
 namespace ara::pipeline {
 
 std::string LogicalPipeline::Explain(const std::vector<Plex>& plexes,
                                      class SinkOp* sink) {
-  return "";
+  std::stringstream ss;
+  for (size_t i = 0; i < plexes.size(); ++i) {
+    if (i > 0) {
+      ss << std::endl;
+    }
+    ss << "Plex" << i << ": " << plexes[i].source_op->Name() << " -> ";
+    for (size_t j = 0; j < plexes[i].pipe_ops.size(); ++j) {
+      ss << plexes[i].pipe_ops[j]->Name() << " -> ";
+    }
+    ss << sink->Name();
+  }
+  return ss.str();
 }
 
 }  // namespace ara::pipeline
--- src/ara/pipeline/physical_pipeline.cpp
+++ src/ara/pipeline/physical_pipeline.cpp
@@ -2,11 +2,23 @@
 #include "logical_pipeline.h"
 
 #include <map>
+#include <sstream>
 
 namespace ara::pipeline {
 
 std::string PhysicalPipeline::Explain(const std::vector<PhysicalPipeline::Plex>& plexes) {
-  return "";
+  std::stringstream ss;
+  for (size_t i = 0; i < plexes.size(); ++i) {
+    if (i > 0) {
+      ss << std::endl;
+    }
+    ss << "Plex" << i << ": " << plexes[i].source_op->Name() << " -> ";
+    for (size_t j = 0; j < plexes[i].pipe_ops.size(); ++j) {
+      ss << plexes[i].pipe_ops[j]->Name() << " -> ";
+    }
+    ss << plexes[i].sink_op->Name();
+  }
+  return ss.str();
 }
 
 namespace {
