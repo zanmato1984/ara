12f61e58bf4713c083eb0518df371ffdb6c4d1d7
zanmato1984 <zanmato1984@gmail.com>
2023-08-29 15:14:49 +0800

    Complete compile pipeline test

--- src/ara/pipeline/pipeline_test.cpp
+++ src/ara/pipeline/pipeline_test.cpp
@@ -38,16 +38,26 @@ class FooSource : public SourceOp {
 
 class FooPipe : public PipeOp {
  public:
-  FooPipe() : PipeOp("FooPipe", "Do nothing") {}
+  FooPipe(std::optional<PipelineDrain> drain = std::nullopt,
+          std::unique_ptr<SourceOp> implicit_source = nullptr)
+      : PipeOp("FooPipe", "Do nothing"),
+        drain_(std::move(drain)),
+        implicit_source_(std::move(implicit_source)) {}
 
   PipelinePipe Pipe() override {
     return [](const PipelineContext&, const TaskContext&, ThreadId,
               std::optional<Batch>) -> OpResult { return OpOutput::PipeEven({}); };
   }
 
-  std::optional<PipelineDrain> Drain() override { return std::nullopt; }
+  std::optional<PipelineDrain> Drain() override { return drain_; }
 
-  std::unique_ptr<SourceOp> ImplicitSource() override { return nullptr; }
+  std::unique_ptr<SourceOp> ImplicitSource() override {
+    return std::move(implicit_source_);
+  }
+
+ private:
+  std::optional<PipelineDrain> drain_;
+  std::unique_ptr<SourceOp> implicit_source_;
 };
 
 class FooSink : public SinkOp {
@@ -71,7 +81,7 @@ class FooSink : public SinkOp {
 TEST(CompilePipelineTest, EmptyPipeline) {
   PipelineContext context;
   FooSink sink;
-  LogicalPipeline logical_pipeline{"EmptyPipeline", {}, &sink};
+  LogicalPipeline logical_pipeline("EmptyPipeline", {}, &sink);
   auto physical_pipelines = CompilePipeline(context, logical_pipeline);
   ASSERT_EQ(physical_pipelines.size(), 0);
 }
@@ -81,132 +91,143 @@ TEST(CompilePipelineTest, SinglePlexPipeline) {
   FooSource source;
   FooPipe pipe;
   FooSink sink;
-  LogicalPipeline logical_pipeline{"SinglePlexPipeline", {{&source, {&pipe}}}, &sink};
+  LogicalPipeline logical_pipeline("SinglePlexPipeline", {{&source, {&pipe}}}, &sink);
   auto physical_pipelines = CompilePipeline(context, logical_pipeline);
   ASSERT_EQ(physical_pipelines.size(), 1);
   ASSERT_TRUE(physical_pipelines[0].ImplicitSources().empty());
-  ASSERT_EQ(physical_pipelines[0].Plexes.size(), 1);
+  ASSERT_EQ(physical_pipelines[0].Plexes().size(), 1);
 }
 
-// TEST(CompilePipelineTest, DoublePlexPipeline) {
-//   size_t dop = 8;
-//   InfiniteSource source_1({}), source_2({});
-//   IdentityPipe pipe;
-//   BlackHoleSink sink;
-//   LogicalPipeline pipeline{{{&source_1, {&pipe}}, {&source_2, {&pipe}}}, &sink};
-//   auto stages = PipelineStageBuilder(pipeline).Build();
-//   ASSERT_EQ(stages.size(), 1);
-//   ASSERT_TRUE(stages[0].sources.empty());
-//   ASSERT_EQ(stages[0].pipeline.plexes.size(), 2);
-// }
+TEST(CompilePipelineTest, DoublePlexPipeline) {
+  PipelineContext context;
+  FooSource source1, source2;
+  FooPipe pipe;
+  FooSink sink;
+  LogicalPipeline logical_pipeline("DoublePlexPipeline",
+                                   {{&source1, {&pipe}}, {&source2, {&pipe}}}, &sink);
+  auto physical_pipelines = CompilePipeline(context, logical_pipeline);
+  ASSERT_EQ(physical_pipelines.size(), 1);
+  ASSERT_TRUE(physical_pipelines[0].ImplicitSources().empty());
+  ASSERT_EQ(physical_pipelines[0].Plexes().size(), 2);
+}
 
-// TEST(CompilePipelineTest, DoubleStagePipeline) {
-//   size_t dop = 8;
-//   InfiniteSource source({});
-//   IdentityWithAnotherSourcePipe pipe(std::make_unique<InfiniteSource>(Batch{}));
-//   BlackHoleSink sink;
-//   LogicalPipeline pipeline{{{&source, {&pipe}}}, &sink};
-//   auto stages = PipelineStageBuilder(pipeline).Build();
-//   ASSERT_EQ(stages.size(), 2);
-//   ASSERT_TRUE(stages[0].sources.empty());
-//   ASSERT_EQ(stages[1].sources.size(), 1);
-//   ASSERT_NE(dynamic_cast<InfiniteSource*>(stages[1].sources[0].get()), nullptr);
-//   ASSERT_EQ(stages[0].pipeline.plexes.size(), 1);
-//   ASSERT_EQ(stages[1].pipeline.plexes.size(), 1);
-// }
+TEST(CompilePipelineTest, DoublePhysicalPipeline) {
+  PipelineContext context;
+  FooSource source;
+  auto implicit_source_ptr = std::make_unique<FooSource>();
+  auto implicit_source = implicit_source_ptr.get();
+  FooPipe pipe(std::nullopt, std::move(implicit_source_ptr));
+  FooSink sink;
+  LogicalPipeline logical_pipeline("DoublePhysicalPipeline", {{&source, {&pipe}}}, &sink);
+  auto physical_pipelines = CompilePipeline(context, logical_pipeline);
+  ASSERT_EQ(physical_pipelines.size(), 2);
+  ASSERT_TRUE(physical_pipelines[0].ImplicitSources().empty());
+  ASSERT_EQ(physical_pipelines[1].ImplicitSources().size(), 1);
+  ASSERT_EQ(physical_pipelines[1].ImplicitSources()[0].get(), implicit_source);
+  ASSERT_EQ(physical_pipelines[0].Plexes().size(), 1);
+  ASSERT_EQ(physical_pipelines[1].Plexes().size(), 1);
+}
 
-// TEST(CompilePipelineTest, DoubleStageDoublePlexPipeline) {
-//   size_t dop = 8;
-//   InfiniteSource source_1({}), source_2({});
-//   IdentityWithAnotherSourcePipe pipe_1(std::make_unique<InfiniteSource>(Batch{})),
-//       pipe_2(std::make_unique<MemorySource>(std::list<Batch>()));
-//   auto pipe_source_1 = pipe_1.source_.get(), pipe_source_2 = pipe_2.source_.get();
-//   BlackHoleSink sink;
-//   LogicalPipeline pipeline{{{&source_1, {&pipe_1}}, {&source_2, {&pipe_2}}}, &sink};
-//   auto stages = PipelineStageBuilder(pipeline).Build();
-//   ASSERT_EQ(stages.size(), 2);
-//   ASSERT_TRUE(stages[0].sources.empty());
-//   ASSERT_EQ(stages[1].sources.size(), 2);
-//   if (stages[1].sources[0].get() == pipe_source_1) {
-//     ASSERT_NE(dynamic_cast<InfiniteSource*>(stages[1].sources[0].get()), nullptr);
-//     ASSERT_NE(dynamic_cast<MemorySource*>(stages[1].sources[1].get()), nullptr);
-//     ASSERT_EQ(stages[1].sources[1].get(), pipe_source_2);
-//   } else {
-//     ASSERT_NE(dynamic_cast<MemorySource*>(stages[1].sources[0].get()), nullptr);
-//     ASSERT_EQ(stages[1].sources[0].get(), pipe_source_2);
-//     ASSERT_NE(dynamic_cast<InfiniteSource*>(stages[1].sources[1].get()), nullptr);
-//     ASSERT_EQ(stages[1].sources[1].get(), pipe_source_1);
-//   }
-//   ASSERT_EQ(stages[0].pipeline.plexes.size(), 2);
-//   ASSERT_EQ(stages[1].pipeline.plexes.size(), 2);
-// }
+TEST(CompilePipelineTest, DoublePhysicalDoublePlexPipeline) {
+  PipelineContext context;
+  FooSource source1, source2;
+  auto implicit_source1_ptr = std::make_unique<FooSource>();
+  auto implicit_source2_ptr = std::make_unique<FooSource>();
+  auto implicit_source1 = implicit_source1_ptr.get();
+  auto implicit_source2 = implicit_source2_ptr.get();
+  FooPipe pipe1(std::nullopt, std::move(implicit_source1_ptr)),
+      pipe2(std::nullopt, std::move(implicit_source2_ptr));
+  FooSink sink;
+  LogicalPipeline logical_pipeline("DoubleDoublePipeline",
+                                   {{&source1, {&pipe1}}, {&source2, {&pipe2}}}, &sink);
+  auto physical_pipelines = CompilePipeline(context, logical_pipeline);
+  ASSERT_EQ(physical_pipelines.size(), 2);
+  ASSERT_TRUE(physical_pipelines[0].ImplicitSources().empty());
+  ASSERT_EQ(physical_pipelines[1].ImplicitSources().size(), 2);
+  if (physical_pipelines[1].ImplicitSources()[0].get() == implicit_source1) {
+    ASSERT_EQ(physical_pipelines[1].ImplicitSources()[1].get(), implicit_source2);
+  } else {
+    ASSERT_EQ(physical_pipelines[1].ImplicitSources()[0].get(), implicit_source2);
+    ASSERT_EQ(physical_pipelines[1].ImplicitSources()[1].get(), implicit_source1);
+  }
+  ASSERT_EQ(physical_pipelines[0].Plexes().size(), 2);
+  ASSERT_EQ(physical_pipelines[1].Plexes().size(), 2);
+}
 
-// TEST(CompilePipelineTest, TrippleStagePipeline) {
-//   size_t dop = 8;
-//   InfiniteSource source_1({}), source_2({});
-//   IdentityWithAnotherSourcePipe pipe_1(std::make_unique<InfiniteSource>(Batch{})),
-//       pipe_2(std::make_unique<MemorySource>(std::list<Batch>())),
-//       pipe_3(std::make_unique<DistributedMemorySource>(dop, std::list<Batch>()));
-//   auto pipe_source_1 = pipe_1.source_.get(), pipe_source_2 = pipe_2.source_.get(),
-//        pipe_source_3 = pipe_3.source_.get();
-//   BlackHoleSink sink;
-//   LogicalPipeline pipeline{
-//       {{&source_1, {&pipe_1, &pipe_3}}, {&source_2, {&pipe_2, &pipe_3}}}, &sink};
-//   auto stages = PipelineStageBuilder(pipeline).Build();
-//   ASSERT_EQ(stages.size(), 3);
-//   ASSERT_TRUE(stages[0].sources.empty());
-//   ASSERT_EQ(stages[1].sources.size(), 2);
-//   ASSERT_EQ(stages[2].sources.size(), 1);
-//   if (stages[1].sources[0].get() == pipe_source_1) {
-//     ASSERT_NE(dynamic_cast<InfiniteSource*>(stages[1].sources[0].get()), nullptr);
-//     ASSERT_NE(dynamic_cast<MemorySource*>(stages[1].sources[1].get()), nullptr);
-//     ASSERT_EQ(stages[1].sources[1].get(), pipe_source_2);
-//   } else {
-//     ASSERT_NE(dynamic_cast<MemorySource*>(stages[1].sources[0].get()), nullptr);
-//     ASSERT_EQ(stages[1].sources[0].get(), pipe_source_2);
-//     ASSERT_NE(dynamic_cast<InfiniteSource*>(stages[1].sources[1].get()), nullptr);
-//     ASSERT_EQ(stages[1].sources[1].get(), pipe_source_1);
-//   }
-//   ASSERT_NE(dynamic_cast<DistributedMemorySource*>(stages[2].sources[0].get()), nullptr);
-//   ASSERT_EQ(stages[2].sources[0].get(), pipe_source_3);
-//   ASSERT_EQ(stages[0].pipeline.plexes.size(), 2);
-//   ASSERT_EQ(stages[1].pipeline.plexes.size(), 2);
-//   ASSERT_EQ(stages[2].pipeline.plexes.size(), 1);
-// }
+TEST(CompilePipelineTest, TripplePhysicalPipeline) {
+  PipelineContext context;
+  FooSource source1, source2;
+  auto implicit_source1_ptr = std::make_unique<FooSource>();
+  auto implicit_source2_ptr = std::make_unique<FooSource>();
+  auto implicit_source3_ptr = std::make_unique<FooSource>();
+  auto implicit_source1 = implicit_source1_ptr.get();
+  auto implicit_source2 = implicit_source2_ptr.get();
+  auto implicit_source3 = implicit_source3_ptr.get();
+  FooPipe pipe1(std::nullopt, std::move(implicit_source1_ptr)),
+      pipe2(std::nullopt, std::move(implicit_source2_ptr)),
+      pipe3(std::nullopt, std::move(implicit_source3_ptr));
+  FooSink sink;
+  LogicalPipeline logical_pipeline(
+      "TripplePhysicalPipeline",
+      {{&source1, {&pipe1, &pipe3}}, {&source2, {&pipe2, &pipe3}}}, &sink);
+  auto physical_pipelines = CompilePipeline(context, logical_pipeline);
+  ASSERT_EQ(physical_pipelines.size(), 3);
+  ASSERT_TRUE(physical_pipelines[0].ImplicitSources().empty());
+  ASSERT_EQ(physical_pipelines[1].ImplicitSources().size(), 2);
+  ASSERT_EQ(physical_pipelines[2].ImplicitSources().size(), 1);
+  if (physical_pipelines[1].ImplicitSources()[0].get() == implicit_source1) {
+    ASSERT_EQ(physical_pipelines[1].ImplicitSources()[1].get(), implicit_source2);
+  } else {
+    ASSERT_EQ(physical_pipelines[1].ImplicitSources()[0].get(), implicit_source2);
+    ASSERT_EQ(physical_pipelines[1].ImplicitSources()[1].get(), implicit_source1);
+  }
+  ASSERT_EQ(physical_pipelines[2].ImplicitSources()[0].get(), implicit_source3);
+  ASSERT_EQ(physical_pipelines[0].Plexes().size(), 2);
+  ASSERT_EQ(physical_pipelines[1].Plexes().size(), 2);
+  ASSERT_EQ(physical_pipelines[2].Plexes().size(), 1);
+}
 
-// TEST(CompilePipelineTest, OddQuadroStagePipeline) {
-//   size_t dop = 8;
-//   InfiniteSource source_1({}), source_2({}), source_3({}), source_4({});
-//   IdentityWithAnotherSourcePipe pipe_1_1(std::make_unique<InfiniteSource>(Batch{})),
-//       pipe_1_2(std::make_unique<InfiniteSource>(Batch{}));
-//   IdentityWithAnotherSourcePipe pipe_2_1(
-//       std::make_unique<MemorySource>(std::list<Batch>()));
-//   IdentityWithAnotherSourcePipe pipe_3_1(
-//       std::make_unique<DistributedMemorySource>(dop, std::list<Batch>()));
-//   BlackHoleSink sink;
-//   LogicalPipeline pipeline{{{&source_1, {&pipe_1_1, &pipe_2_1, &pipe_3_1}},
-//                             {&source_2, {&pipe_2_1, &pipe_3_1}},
-//                             {&source_3, {&pipe_1_2, &pipe_3_1}},
-//                             {&source_4, {&pipe_3_1}}},
-//                            &sink};
-//   auto stages = PipelineStageBuilder(pipeline).Build();
+TEST(CompilePipelineTest, OddQuadroStagePipeline) {
+  PipelineContext context;
+  FooSource source1, source2, source3, source4;
+  auto implicit_source1_ptr = std::make_unique<FooSource>();
+  auto implicit_source2_ptr = std::make_unique<FooSource>();
+  auto implicit_source3_ptr = std::make_unique<FooSource>();
+  auto implicit_source4_ptr = std::make_unique<FooSource>();
+  auto implicit_source1 = implicit_source1_ptr.get();
+  auto implicit_source2 = implicit_source2_ptr.get();
+  auto implicit_source3 = implicit_source3_ptr.get();
+  auto implicit_source4 = implicit_source4_ptr.get();
+  FooPipe pipe1(std::nullopt, std::move(implicit_source1_ptr)),
+      pipe2(std::nullopt, std::move(implicit_source2_ptr)),
+      pipe3(std::nullopt, std::move(implicit_source3_ptr)),
+      pipe4(std::nullopt, std::move(implicit_source4_ptr));
+  FooSink sink;
+  LogicalPipeline logical_pipeline("OddQuadroStagePipeline",
+                                   {{&source1, {&pipe1, &pipe2, &pipe4}},
+                                    {&source2, {&pipe2, &pipe4}},
+                                    {&source3, {&pipe3, &pipe4}},
+                                    {&source4, {&pipe4}}},
+                                   &sink);
+  auto physical_pipelines = CompilePipeline(context, logical_pipeline);
+  ASSERT_EQ(physical_pipelines.size(), 4);
+  ASSERT_TRUE(physical_pipelines[0].ImplicitSources().empty());
+  ASSERT_EQ(physical_pipelines[1].ImplicitSources().size(), 2);
+  ASSERT_EQ(physical_pipelines[2].ImplicitSources().size(), 1);
+  ASSERT_EQ(physical_pipelines[3].ImplicitSources().size(), 1);
 
-//   ASSERT_EQ(stages.size(), 4);
-//   ASSERT_TRUE(stages[0].sources.empty());
-//   ASSERT_EQ(stages[1].sources.size(), 2);
-//   ASSERT_EQ(stages[2].sources.size(), 1);
-//   ASSERT_EQ(stages[3].sources.size(), 1);
+  if (physical_pipelines[1].ImplicitSources()[0].get() == implicit_source1) {
+    ASSERT_EQ(physical_pipelines[1].ImplicitSources()[1].get(), implicit_source3);
+  } else {
+    ASSERT_EQ(physical_pipelines[1].ImplicitSources()[0].get(), implicit_source3);
+    ASSERT_EQ(physical_pipelines[1].ImplicitSources()[1].get(), implicit_source1);
+  }
 
-//   ASSERT_NE(dynamic_cast<InfiniteSource*>(stages[1].sources[0].get()), nullptr);
-//   ASSERT_NE(dynamic_cast<InfiniteSource*>(stages[1].sources[1].get()), nullptr);
-//   ASSERT_NE(dynamic_cast<MemorySource*>(stages[2].sources[0].get()), nullptr);
-//   ASSERT_NE(dynamic_cast<DistributedMemorySource*>(stages[3].sources[0].get()), nullptr);
-
-//   ASSERT_EQ(stages[0].pipeline.plexes.size(), 4);
-//   ASSERT_EQ(stages[1].pipeline.plexes.size(), 2);
-//   ASSERT_EQ(stages[2].pipeline.plexes.size(), 1);
-//   ASSERT_EQ(stages[3].pipeline.plexes.size(), 1);
-// }
+  ASSERT_EQ(physical_pipelines[0].Plexes().size(), 4);
+  ASSERT_EQ(physical_pipelines[1].Plexes().size(), 2);
+  ASSERT_EQ(physical_pipelines[2].Plexes().size(), 1);
+  ASSERT_EQ(physical_pipelines[3].Plexes().size(), 1);
+}
 
 TEST(PipelineTaskTest, TaskBasic) {
   PipelineContext pipeline_context;
